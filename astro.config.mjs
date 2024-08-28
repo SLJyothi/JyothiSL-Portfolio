b35d7b11cd8e660e41494ca058596f6e8d195fde
SLJyothi <164232591+SLJyothi@users.noreply.github.com>
2024-08-29 00:06:56 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+
+
+
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'synthwave-84',
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+  renderers: ['@astrojs/renderer-react'],
+  site: 'https://itssofi.dev/',
+  integrations: [mdx(), sitemap(), react()]
+});
